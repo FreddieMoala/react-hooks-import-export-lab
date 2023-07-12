1f9339e40641dcfe606c4ac4be45d691b048fca7
Freddie Moala <loto_toa@hotmail.com>
2023-07-12 23:07:40 +1000

    Done-Again

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Home from "./Home";
+import About from "./About";
 
 export default function NavBar() {
     return ( 
       <nav>
-        <a href = "#home" > I 'm a link!</a> 
+        <a href = "#Home" > Home</a> 
+        <a href = "#About" > About</a> 
       </nav>
     );
 }
